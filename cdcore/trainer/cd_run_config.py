02b80ee66a43000af37536cc19bb50255ffddd2d
Osmau2004 <1153510039@qq.com>
2025-03-04 21:48:01 +0800

    清空 main 分支的文件

--- cdcore/trainer/cd_run_config.py
+++
@@ -1,9 +0,0 @@
-from apps.trainer.run_config import RunStepConfig
-
-__all__ = ["CDRunConfig"]
-
-
-class CDRunConfig(RunStepConfig):
-    @property
-    def none_allowed(self):
-        return [] + super().none_allowed
--- segcore/trainer/seg_run_config.py
+++
@@ -1,9 +0,0 @@
-from apps.trainer.run_config import RunStepConfig
-
-__all__ = ["SegRunConfig"]
-
-
-class SegRunConfig(RunStepConfig):
-    @property
-    def none_allowed(self):
-        return [] + super().none_allowed
