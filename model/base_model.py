1683514629a00f28949f16aee5e9823384b6ea3a
Osmau2004 <1153510039@qq.com>
2025-03-17 10:39:59 +0800

    add model

--- model/base_model.py
+++ model/base_model.py
@@ -1,13 +1,22 @@
-import torch.nn as nn
-from abc import ABCMeta
-from typing import Iterable, List, Optional, Union
-import copy
+from model.base_module import BaseModule
 
-class BaseModule(nn.Module, metaclass=ABCMeta):
-    def __init__(self, init_cfg: Union[dict, List[dict], None] = None):
-        super().__init__()
-        # define default value of init_cfg instead of hard code
-        # in init_weights() function
-        self._is_init = False
-
-        self.init_cfg = copy.deepcopy(init_cfg)
+class BaseModel(BaseModule):
+    def __init__(self,
+                 data_preprocessor: Optional[Union[dict, nn.Module]] = None,
+                 init_cfg: Optional[dict] = None):
+        super().__init__(init_cfg)
+        if data_preprocessor is None:
+            data_preprocessor = dict(type='BaseDataPreprocessor')
+        if isinstance(data_preprocessor, nn.Module):
+            self.data_preprocessor = data_preprocessor
+        elif isinstance(data_preprocessor, dict):
+            self.data_preprocessor = MODELS.build(data_preprocessor)
+        else:
+            raise TypeError('data_preprocessor should be a `dict` or '
+                            f'`nn.Module` instance, but got '
+                            f'{type(data_preprocessor)}')
+    @abstractmethod
+    def forward(self,
+                inputs: torch.Tensor,
+                data_samples: Optional[list] = None,
+                mode: str = 'tensor') -> Union[Dict[str, torch.Tensor], list]:
