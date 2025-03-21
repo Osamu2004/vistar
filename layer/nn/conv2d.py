f53617036a9f549c1bd954a09c946edcd944c566
Osmau2004 <1153510039@qq.com>
2025-03-21 18:27:24 +0800

    renew layer

--- layer/nn/conv2d.py
+++ layer/nn/conv2d.py
@@ -46,3 +46,45 @@ class ConvNormAct(nn.Module):
         if self.act:
             x = self.act(x)
         return x
+    
+class ConvBn(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, padding_mode='zeros', deploy=False):
+        super(ConvBn, self).__init__()
+        self.deploy = deploy
+        if deploy:
+            self.fused_conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(kernel_size,kernel_size), stride=stride,
+                                      padding=padding, dilation=dilation, groups=groups, bias=True, padding_mode=padding_mode)
+        else:
+            self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                         kernel_size=(kernel_size, kernel_size), stride=stride,
+                                         padding=padding, dilation=dilation, groups=groups, bias=False,
+                                         padding_mode=padding_mode)
+            self.bn = nn.BatchNorm2d(num_features=out_channels)
+
+
+    def _fuse_bn_tensor(self, conv, bn):
+        std = (bn.running_var + bn.eps).sqrt()
+        t = (bn.weight / std).reshape(-1, 1, 1, 1)
+        return conv.weight * t, bn.bias - bn.running_mean * bn.weight / std
+
+
+    def switch_to_deploy(self):
+        deploy_k, deploy_b = self._fuse_bn_tensor(self.conv, self.bn)
+        self.deploy = True
+        self.fused_conv = nn.Conv2d(in_channels=self.conv.in_channels, out_channels=self.conv.out_channels,
+                                    kernel_size=self.conv.kernel_size, stride=self.conv.stride,
+                                    padding=self.conv.padding, dilation=self.conv.dilation, groups=self.conv.groups, bias=True,
+                                    padding_mode=self.conv.padding_mode)
+        self.__delattr__('conv')
+        self.__delattr__('bn')
+        self.fused_conv.weight.data = deploy_k
+        self.fused_conv.bias.data = deploy_b
+
+    def forward(self, input):
+        if self.deploy:
+            return self.fused_conv(input)
+        else:
+            square_outputs = self.conv(input)
+            square_outputs = self.bn(square_outputs)
+            return square_outputs
